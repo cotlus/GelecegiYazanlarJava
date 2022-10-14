e25f0740457b1548de0a8c62978d8844f08dd697
cotlus <emre@cotlus.com>
2022-10-14 22:06:29 +0300

    Upload

---
+++ Homework-4/README.md
@@ -0,0 +1,2 @@
+# Tree Info
+This Java app is made quite simply for the inheritance example.
---
+++ Homework-4/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        IgneYaprakli ahmet = new IgneYaprakli("Sarıçam", false, true, 2, 6, "a");
+        ahmet.agacBilgi();
+    }
+}
