95067598fb7becb93bba530866aa93dabda23a41
Emre <96750949+cotlus@users.noreply.github.com>
2022-10-14 22:07:27 +0300

    Delete Homework-4 directory

--- Homework-4/README.md
+++
@@ -1,2 +0,0 @@
-# Tree Info
-This Java app is made quite simply for the inheritance example.
--- Homework-4/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        IgneYaprakli ahmet = new IgneYaprakli("Sarıçam", false, true, 2, 6, "a");
-        ahmet.agacBilgi();
-    }
-}
