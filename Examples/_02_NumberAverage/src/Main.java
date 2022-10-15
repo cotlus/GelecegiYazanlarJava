02d6e7ed9fb1232800eb52a58d43e5fed1bad0e0
cotlus <emre@cotlus.com>
2022-10-15 12:38:17 +0300

    Update NumberAverage

--- Examples/_02_NumberAverage/src/Main.java
+++ Examples/_02_NumberAverage/src/Main.java
@@ -2,19 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Lütfen ilk sayıyı giriniz:");
+        System.out.println("Please enter the first number:");
         int girdiA = new Scanner(System.in).nextInt();
 
-        System.out.println("Lütfen ikinci sayıyı giriniz:");
+        System.out.println("Please enter the second number:");
         int girdiB = new Scanner(System.in).nextInt();
 
-        System.out.println("Lütfen üçüncü sayıyı giriniz:");
+        System.out.println("Please enter the third number:");
         int girdiC = new Scanner(System.in).nextInt();
 
-        System.out.println("Lütfen dördüncü sayıyı giriniz:");
+        System.out.println("Please enter the fourth number:");
         int girdiD = new Scanner(System.in).nextInt();
         int toplam = girdiA + girdiB + girdiC + girdiD;
-        System.out.println("Sayıların toplamı: " + toplam);
-        System.out.println("Sayıların ortalaması: " + toplam / 4);
+        System.out.println("Sum of numbers:: " + toplam);
+        System.out.println("Average of numbers: " + toplam / 4);
     }
 }
